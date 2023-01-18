ce8ba0f65c59ee0009c48518028423a7e47d919d
Alejandro <alejandro-243@hotmail.com>
2023-01-18 13:36:33 -0500

    Added api for the product CRUD

--- product/views.py
+++ product/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.core.mail import send_mail
+from rest_framework import viewsets, permissions
+from product.models import Product
+from rest_framework.response import Response
+from product.serializers import ProductSerializer
 
-# Create your views here.
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.filter(status=True)
+    serializer_class = ProductSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.called += 1
+        instance.save()
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
+
+    def create(self, request, *args, **kwargs):
+        response = super().create(request, *args, **kwargs)
+
+        product = Product.objects.get(pk=response.data['id'])
+        # Todo agregar todos los admin activos
+        send_mail(
+            'New Product Created',
+            f'A new product with name {product.name} and id {product.id} has been created.',
+            'from@example.com',
+            ['to@example.com'],
+            fail_silently=False,
+        )
+
+        return response
